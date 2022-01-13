1c1f98fb520996b9cbb9a52c269b6b264cf3cd33
issuarez <issuarez@aidacanarias.com>
2022-01-13 18:46:31 +0100

    ♻ Change visibility Questions properties

--- C#/Trivia/Trivia/Questions.cs
+++ C#/Trivia/Trivia/Questions.cs
@@ -6,10 +6,10 @@ namespace Trivia
 {
 	public class Questions
 	{
-		public readonly LinkedList<string> _popQuestions     = new LinkedList<string>();
-		public readonly LinkedList<string> _scienceQuestions = new LinkedList<string>();
-		public readonly LinkedList<string> _sportsQuestions  = new LinkedList<string>();
-		public readonly LinkedList<string> _rockQuestions    = new LinkedList<string>();
+		private readonly LinkedList<string> _popQuestions     = new LinkedList<string>();
+		private readonly LinkedList<string> _scienceQuestions = new LinkedList<string>();
+		private readonly LinkedList<string> _sportsQuestions  = new LinkedList<string>();
+		private readonly LinkedList<string> _rockQuestions    = new LinkedList<string>();
 
 		public Questions()
 		{
