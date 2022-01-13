10532dacdd986ad905164663f0bb629da0751dca
issuarez <issuarez@aidacanarias.com>
2022-01-13 21:34:40 +0100

    ♻ Enum Category to enumeration class

--- C#/Trivia/Trivia/Category.cs
+++ C#/Trivia/Trivia/Category.cs
@@ -1,10 +1,20 @@
-﻿namespace Trivia
+﻿using System.Collections.Generic;
+
+namespace Trivia
 {
-	public enum Category
+	public class Category
 	{
-		Pop,
-		Science,
-		Sports,
-		Rock
+		public static Category Pop     { get; } = new Category("Pop");
+		public static Category Science { get; } = new Category("Science");
+		public static Category Sports  { get; } = new Category("Sports");
+		public static Category Rock    { get; } = new Category("Rock");
+
+		private readonly string   _description;
+
+		private Category(string description) => this._description = description;
+
+		public static IEnumerable<Category> Categories { get; } = new List<Category>() { Pop, Science, Sports, Rock};
+
+		public override string ToString() => this._description;
 	}
 }
