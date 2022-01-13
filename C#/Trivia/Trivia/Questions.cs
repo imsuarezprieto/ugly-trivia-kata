7bd28fbc520d4531156072b0ecb60dd5f25281a3
issuarez <issuarez@aidacanarias.com>
2022-01-13 21:47:33 +0100

    ♻ Helper methods in Questions class

--- C#/Trivia/Trivia/Questions.cs
+++ C#/Trivia/Trivia/Questions.cs
@@ -6,19 +6,24 @@ namespace Trivia
 {
 	public class Questions
 	{
+		private const int nQuestionsPerCategory = 50;
+
 		private readonly Dictionary<Category, Queue<string>> _questions =
 				new Dictionary<Category, Queue<string>>(
-						Category.Categories
-								.Select(category =>
-										new KeyValuePair<Category, Queue<string>>(
-												category,
-												new Queue<string>(
-														Enumerable.Range(0, 50)
-																.Select(n => $"{category} Question {n}")
-												)
-										)
-								)
+						Category.Categories.Select(CategoryQuestions)
 				);
+
+		private static KeyValuePair<Category, Queue<string>> CategoryQuestions(Category category) =>
+				new KeyValuePair<Category, Queue<string>>(
+						category,
+						new Queue<string>(
+								QuestionDeck(category)
+						)
+				);
+
+		private static IEnumerable<string> QuestionDeck(Category category) =>
+				Enumerable.Range(0, nQuestionsPerCategory)
+						.Select(n => $"{category} Question {n}");
 
 		public void AskQuestion(Category category) =>
 				Console.WriteLine(
