b69c39804a22d30d6ca76bd382729f7e9f2a22d9
issuarez <issuarez@aidacanarias.com>
2022-01-13 17:58:45 +0100

    ✅ Golden master generator

--- C#/Trivia/Trivia.Tests/GameTest.cs
+++ C#/Trivia/Trivia.Tests/GameTest.cs
@@ -1,12 +1,31 @@
+using System;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Trivia.Tests
 {
-    public class GameTest
-    {
-        [Fact]
-        public void Test1()
-        {
-        }
-    }
+	public class GameTest
+	{
+		[Fact]
+		public void GenerateGoldenMaster()
+		{
+			const string golderMasterFolder = @"../../../../GoldenMaster";
+			Directory.CreateDirectory(golderMasterFolder);
+			foreach (int randomSeed in Enumerable.Range(0, 4000))
+			{
+				using StreamWriter output =
+						new StreamWriter(
+								new FileStream(
+										$@"{golderMasterFolder}/Output-{randomSeed:0000}.txt",
+										FileMode.Create
+								)
+						);
+				Console.SetOut(output);
+				GameRunner.RunGame(
+						new Random(randomSeed)
+				);
+			}
+		}
+	}
 }
