7788f0305b54a8ebcca7970bf3d689f39b9c742f
issuarez <issuarez@aidacanarias.com>
2022-01-13 21:54:00 +0100

    ♻ Question deck type alias

--- C#/Trivia/Trivia/Questions.cs
+++ C#/Trivia/Trivia/Questions.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using Question = System.String;
+using Deck = System.Collections.Generic.Queue<string>;
+
 namespace Trivia
 {
 	public class Questions
 	{
 		private const int nQuestionsPerCategory = 50;
 
-		private readonly Dictionary<Category, Queue<string>> _questions =
-				new Dictionary<Category, Queue<string>>(
+		private readonly Dictionary<Category, Deck> _questions =
+				new Dictionary<Category, Deck>(
 						Category.Categories.Select(CategoryQuestions)
 				);
 
-		private static KeyValuePair<Category, Queue<string>> CategoryQuestions(Category category) =>
-				new KeyValuePair<Category, Queue<string>>(
+		private static KeyValuePair<Category, Deck> CategoryQuestions(Category category) =>
+				new KeyValuePair<Category, Deck>(
 						category,
-						new Queue<string>(
+						new Deck(
 								QuestionDeck(category)
 						)
 				);
 
-		private static IEnumerable<string> QuestionDeck(Category category) =>
+		private static IEnumerable<Question> QuestionDeck(Category category) =>
 				Enumerable.Range(0, nQuestionsPerCategory)
 						.Select(n => $"{category} Question {n}");
 
