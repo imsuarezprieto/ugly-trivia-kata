659a698d922ee0e399d36d32d503268abfbcdd9a
issuarez <issuarez@aidacanarias.com>
2022-01-14 14:52:23 +0100

    ♻ Introduce Board/Place class

---
+++ C#/Trivia/Trivia/Board.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Trivia
+{
+	public class Board
+	{
+		private static readonly IList<Place> _places = new List<Place>()
+		{
+				new Place(Category.Pop),
+				new Place(Category.Science),
+				new Place(Category.Sports),
+				new Place(Category.Rock),
+				new Place(Category.Pop),
+				new Place(Category.Science),
+				new Place(Category.Sports),
+				new Place(Category.Rock),
+				new Place(Category.Pop),
+				new Place(Category.Science),
+				new Place(Category.Sports),
+				new Place(Category.Rock),
+		};
+
+		public static Place InitialPlace => _places[0];
+
+		public class Place
+		{
+			public Category Category { get; }
+
+			public Place(Category category)
+			{
+				this.Category = category;
+			}
+
+			public Place Advance(int places)
+			{
+				return _places[(_places.IndexOf(this) + places) % _places.Count];
+			}
+		}
+	}
+}
