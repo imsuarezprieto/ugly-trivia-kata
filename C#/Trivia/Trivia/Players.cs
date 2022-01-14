abc5dab95687315d1197e6e59087cae2b0271edf
issuarez <issuarez@aidacanarias.com>
2022-01-14 08:24:45 +0100

    ♻ Introduce Players class

---
+++ C#/Trivia/Trivia/Players.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trivia
+{
+	public class Players
+	{
+		private readonly List<Player> _players = new List<Player>();
+		private          int          current  = 0;
+
+		public Player Current => this._players[current];
+
+		public void Add(string playerName)
+		{
+			this._players.Add(new Player(playerName));
+
+			Console.WriteLine($"{playerName} was added");
+			Console.WriteLine($"They are player number {_players.Count}");
+		}
+
+		public void Next()
+		{
+			this.current = ++this.current % this._players.Count;
+		}
+	}
+}
