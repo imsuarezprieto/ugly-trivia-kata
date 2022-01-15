00e3ff4b5e52ac938119b7aedaaad7f26ae8846f
issuarez <issuarez@aidacanarias.com>
2022-01-15 07:59:01 +0100

    ♻ Extrac next player game logic

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -6,64 +6,68 @@ namespace Trivia
 {
 	public class Game
     {
+	    private          Player    currentPlayer;
 	    private readonly Players   _players  = new Players();
 	    private readonly Questions questions = new Questions();
 
-        public void Add(string playerName)
+	    public void Add(string playerName)
         {
             _players.Add(playerName);
         }
 
         public void Roll(int roll)
         {
-	        this._players.Next();
+	        NexPlayer();
             Console.WriteLine("They have rolled a " + roll);
 
-            if (_players.Current.IsInPenaltyBox != null)
-            {
-	            if (roll % 2 == 0)
-	            {
-		          _players.Current.StayInPenaltyBox();
-		          return;
-	            }
-	            else
-	            {
-		            _players.Current.GetOutPenaltyBox();
-	            }
-            }
+            if (currentPlayer.IsInPenaltyBox != null) 
+	            CheckPenaltyBox(roll);
 
-	        Advance(roll);
+            Advance(roll);
 	        AskQuestion();
+        }
+
+        private void NexPlayer()
+        {
+	        this._players.Next();
+	        this.currentPlayer = _players.Current;
+        }
+
+        private void CheckPenaltyBox(int roll)
+        {
+	        if (roll % 2 == 0)
+		        currentPlayer.StayInPenaltyBox();
+	        else
+		        currentPlayer.GetOutPenaltyBox();
         }
 
         private void AskQuestion()
         {
-	        if (_players.Current.IsInPenaltyBox ?? false) return;
+	        if (currentPlayer.IsInPenaltyBox ?? false) return;
 	        questions.AskQuestion(
-			        _players.Current.Place.Category);
+			        currentPlayer.Place.Category);
         }
 
         private void Advance(int roll)
         {
-	        if (_players.Current.IsInPenaltyBox ?? false) return;
-	        _players.Current.Advance(roll);
+	        if (currentPlayer.IsInPenaltyBox ?? false) return;
+	        currentPlayer.Advance(roll);
         }
 
         public void WasCorrectlyAnswered()
         {
-	        if (_players.Current.IsInPenaltyBox ?? false) return;
+	        if (currentPlayer.IsInPenaltyBox ?? false) return;
 	        Console.WriteLine("Answer was correct!!!!");
-	        _players.Current.AddCoin();
+	        currentPlayer.AddCoin();
         }
 
         public void WrongAnswer()
         {
             Console.WriteLine("Question was incorrectly answered");
-            _players.Current.GoToPenaltyBox();
+            currentPlayer.GoToPenaltyBox();
         }
 
-
-		public bool HasWinner => _players.Current.HasFullPurse;
+        public bool HasWinner => currentPlayer.HasFullPurse;
 	}
 
 }
